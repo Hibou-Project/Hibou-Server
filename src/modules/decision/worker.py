93dfdd67cfee0f8c3c3223f8aa46bb1abe53b745
n1coc4cola <n1coc4cola@gmail.com>
2026-04-01 15:25:59 +0200

    Add simple implementataion of decision algorithm

--- src/modules/decision/worker.py
+++ src/modules/decision/worker.py
@@ -3,9 +3,15 @@ from src.helpers.decorators import SingletonMeta
 from src.logger import CustomLogger
 from src.settings import SETTINGS
 import datetime
-import time
 import os
-import numpy as np
+from collections import deque
+import threading
+from src.helpers.json import read_json
+from pathlib import Path
+
+from src.modules.audio.localization.data import MicInfo
+from src.modules.decision.decision import DecisionMaker
+
 
 logger = CustomLogger("decision").get_logger()
 
@@ -18,33 +24,51 @@ class DecisionWorker:
         self.ipc.subscribe(SETTINGS.IPC_ACOUSTIC_DETECTION_TOPIC, self._update_audio_inf)
         self.ipc.subscribe(SETTINGS.IPC_ACOUSTIC_ANGLE_TOPIC, self._update_audio_angle)
 
-        self.angle = 0.0
-        self.inf = []
+        self.angles = deque(maxlen=3)
+        self.inferences = deque(maxlen=3)
 
-        self._reset = True
+        self._updated = False
+        self._condition = threading.Condition()
+
+        mic_data = read_json(Path("./mic_information.json"))
+        mic_array = mic_data["array"]
+        self.mic_infos = [
+            MicInfo.from_dict(info)
+            for info in mic_array
+        ]
+
+        self._dm = DecisionMaker(self.angles, self.inferences, self.mic_infos, mic_data["opening"])
 
         try:
             self.run()
         except KeyboardInterrupt:
-            logger.critical("Stopping Audio Worker...")
+            logger.critical("Stopping Decision Worker...")
         finally:
             SingletonMeta.clear()
 
-    def _update_audio_inf(self, values):
-        self.inf = values
+    def _update_audio_inf(self, topic: str, values: str):
+        predictions = [bool(v) for v in values.split(",") if len(v) != 0]
 
-    def _update_audio_angle(self, angle):
-        self.angle = angle
+        with self._condition:
+            self.inferences.append(predictions)
+            self._updated = True
+            self._condition.notify()
+
+    def _update_audio_angle(self, topic: str, angle: str):
+        with self._condition:
+            self.angles.append(float(angle))
+            self._updated = True
+            self._condition.notify()
 
     def run(self):
         logger.info("Running decision worker...")
         while True:
-            time.sleep(0.01)
-            if np.any(self.inf) or self.angle != 0.0:
-                self._reset = True
-                print(self.inf)
-                print(self.angle)
-                # [TODO] Add logic here
-            elif self._reset:
-                self._reset = False
-                print("Waiting for angle update...")
+            with self._condition:
+                while not self._updated or len(self.inferences) < 3 or len(self.angles) < 3:
+                    self._condition.wait()
+
+                self._updated = False
+
+                angle = self._dm.update()
+                self.ipc.publish(SETTINGS.IPC_DECISION_ANGLE_TOPIC, f"{angle}")
+                print(angle)
